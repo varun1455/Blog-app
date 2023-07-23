b70db7eb840075416ccf035169035d7ee3a56740
Varun Jain <83868029+varun1455@users.noreply.github.com>
2023-07-23 22:26:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+import Topbar from "./components/topbar/Topbar";
+import Homepage from "./pages/homepage/Homepage";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import Settings from "./pages/settings/Settings";
+import Single from "./pages/single/Single";
+import Write from "./pages/write/Write";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+
 
 function App() {
+  const currentUser = true;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {/* <Register/> */}
+    {/* <Login/> */}
+    <Router>
+      <Topbar/>
+      
+      
+      <Routes>
+        <Route exact path="/" element={<Homepage />}/>
+          
+        </Routes>
+        <Routes>
+        <Route path="/posts" element={<Homepage />}/>
+          
+        </Routes>
+        <Routes>
+        <Route path="/register" element={currentUser ? (<Register />): (<Navigate replace={true} to = {'/'}/>) } />
+          
+        </Routes>
+        <Routes>
+        <Route path="/login"  element={currentUser ? (<Login />): (<Navigate replace={true}  to = {'/'}/>) } />
+       </Routes>
+       <Routes>
+        <Route path="/post/:id" element={<Single />}/>
+          
+        </Routes>
+        <Routes>
+        <Route path="/write" element={currentUser ? ( <Write /> ): (<Navigate replace={true} to = {'/login'}/>)} />
+        </Routes>
+        <Routes>
+        
+        <Route path="/settings" element={currentUser ? (<Settings /> ) : (<Navigate replace={true}  to = {'/login'}/>)}/>
+          
+        </Routes>
+     
+    </Router>
+    </>
   );
 }
 
 export default App;
+
---
+++ src/components/sidebar/Sidebar.jsx
@@ -0,0 +1,64 @@
+import { Link } from "react-router-dom";
+import "./sidebar.css";
+
+export default function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebarItem">
+        <span className="sidebarTitle">ABOUT ME</span>
+        <img
+          src="https://images.unsplash.com/photo-1537511446984-935f663eb1f4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1170&q=80"
+          alt=""
+        />
+        <p>
+          Laboris sunt aute cupidatat velit magna velit ullamco dolore mollit
+          amet ex esse.Sunt eu ut nostrud id quis proident.
+        </p>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">CATEGORIES</span>
+        <ul className="sidebarList">
+          <li className="sidebarListItem">
+            <Link className="link" to="/posts?cat=Life">
+              Life
+            </Link>
+          </li>
+          <li className="sidebarListItem">
+            <Link className="link" to="/posts?cat=Music">
+              Music
+            </Link>
+          </li>
+          <li className="sidebarListItem">
+            <Link className="link" to="/posts?cat=Sport">
+              Sport
+            </Link>
+          </li>
+          <li className="sidebarListItem">
+            <Link className="link" to="/posts?cat=Style">
+              Style
+            </Link>
+          </li>
+          <li className="sidebarListItem">
+            <Link className="link" to="/posts?cat=Tech">
+              Tech
+            </Link>
+          </li>
+          <li className="sidebarListItem">
+            <Link className="link" to="/posts?cat=Cinema">
+              Cinema
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div className="sidebarItem">
+        <span className="sidebarTitle">FOLLOW US</span>
+        <div className="sidebarSocial">
+          <i className="sidebarIcon fab fa-facebook-square"></i>
+          <i className="sidebarIcon fab fa-instagram-square"></i>
+          <i className="sidebarIcon fab fa-pinterest-square"></i>
+          <i className="sidebarIcon fab fa-twitter-square"></i>
+        </div>
+      </div>
+    </div>
+  );
+}
